cb91e1bddbc4c8efd2d5e7a45f0da0eff454fb4b
Mistchenko <aleksandr@mistchenko.ru>
2021-04-04 12:04:40 +0300

    Model Note

--- notepad/note/models.py
+++ notepad/note/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=160, verbose_name='Наименование блокнота')
+    text = models.TextField(default='', blank=True, verbose_name='Текст')
+    author = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='Автор')
+
+    def __str__(self):
+        return self.title
